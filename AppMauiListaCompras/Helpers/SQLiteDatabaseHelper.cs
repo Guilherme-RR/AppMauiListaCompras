d6be1d4585fabfc5d533ecbe1a92f2d6dda1d799
Guilherme-RR <guilhermerafaelrosa89@gmail.com>
2024-03-07 15:03:58 -0300

    Add formulário para ovo produto e término da Imagem

--- AppMauiListaCompras/Helpers/SQLiteDatabaseHelper.cs
+++ AppMauiListaCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -32,6 +32,17 @@ namespace AppMauiListaCompras.Helpers
             return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Preco, p.Quantidade, p.Id); 
         }
 
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = "SELECT * FROM Produto WHERE " + "descricao LIKE '%" + q + "%'";
+            return _conn.QueryAsync<Produto>(sql);
+        }
+
         public Task<int> Delete(int id)
         {
             return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
